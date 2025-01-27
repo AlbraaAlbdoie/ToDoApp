cc773bec085605f3711c0b95f25272cf0ef10b33
Albraa Adel Albdoie <AlbraaAlbdoie@gmail.com>
2025-01-27 16:00:46 +0300

    create dialog box

--- lib/pages/ToDotile.dart
+++ lib/pages/ToDotile.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class ToDotile extends StatelessWidget {
-  const ToDotile({super.key});
+  ToDotile(
+      {super.key,
+      required this.taskName,
+      required this.taskcompleted,
+      required this.onchinge});
+  final String taskName;
+  final bool taskcompleted;
+  Function(bool?)? onchinge;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            color: Colors.deepPurple),
+        child: Row(
+          children: [
+            Checkbox(
+                side: BorderSide(color: Colors.white, width: 2),
+                checkColor: Colors.white,
+                activeColor: Colors.deepPurpleAccent[500],
+                value: taskcompleted,
+                onChanged: onchinge),
+            // task name
+            Text(
+              taskName,
+              style: TextStyle(
+                  decoration: taskcompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                  decorationColor: Colors.white,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,27 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/pages/ToDotile.dart';
+import 'package:todo_app/pages/dialog_box.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  List todolist = [
+    ['Stading', false],
+    ['playing', false],
+  ];
+  void chackboxchang(bool? value, int index) {
+    setState(() {
+      todolist[index][1] = !todolist[index][1];
+    });
+  }
+
+  void createnewtask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox();
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[300],
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'TO DO ',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        backgroundColor: Colors.deepPurple[300],
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.deepPurple,
+          onPressed: createnewtask,
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
           ),
         ),
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: ListView(
-        children: [
-          ToDotile()
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'TO DO ',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: ListView.builder(
+            itemCount: todolist.length,
+            itemBuilder: (context, index) {
+              return ToDotile(
+                taskName: todolist[index][0],
+                taskcompleted: todolist[index][1],
+                onchinge: (value) => chackboxchang(value, index),
+              );
+            }));
   }
 }
